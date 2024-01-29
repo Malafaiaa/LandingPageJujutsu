ebf205190fb4d5c0435204563d6caf3ce4c459a1
Guilherme Malafaia <guiihmalafaia23@gmail.com>
2024-01-28 23:09:00 -0300

    Alterações

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const botao = document.querySelector(".btn-plataforma");
+    const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas");
 
+    botao.addEventListener("click", () => {
+        elementoPlataformas.classList.toggle("ativo");
+    });
 
-const botao = document.querySelector(".btn-plataforma")
-const elementoPlataformas = document.querySelector(".btn-plataforma .plataformas ")
-
-
-botao.addEventListener("click", () => {
-
-
-elementoPlataformas.classList.toggle("ativo")
-
-
-
-})
-
-
+    const inputsPlataformas = document.querySelectorAll(".plataformas input");
+    inputsPlataformas.forEach(input => {
+        input.addEventListener("click", () => {
+            const linkDaPlataforma = input.getAttribute("data-link");
+            window.open(linkDaPlataforma, '_blank'); // Abre o link em uma nova guia
+        });
+    });
+});
